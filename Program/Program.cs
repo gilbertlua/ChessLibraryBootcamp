89c12847ef7134fc3577bb5823ec94297dac1774
gilbertlua <gilbertgmtidn@gmail.com>
2023-09-05 11:49:51 +0700

    add program

---
+++ Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using ChessLibrary;
+
+class Program{
+    static GameController gameController = new GameController();
+    
+    static void Main(){
+        Program program = new();
+        program.GenerateBoard();
+    }
+    void GenerateBoard(){
+        Piece[,] pieces = gameController.GetBoard();
+
+        foreach(Piece piece in pieces){
+            Console.WriteLine(piece.GetName());
+        }
+    }
+}
